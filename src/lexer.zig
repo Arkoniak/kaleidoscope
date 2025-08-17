bd2a2f959e0ffc429a234796a1ff03aff296b0e5
Arkoniak <none@of.your.business>
2025-08-18 00:33:25 +0300

    updated lexer

--- src/lexer.zig
+++ src/lexer.zig
@@ -10,11 +10,12 @@ const Tag = enum {
     def,
     tag_extern,
 
-    //primary
+    // primary
     identifier,
     number,
 
-    regular,
+    // everything else
+    unknown,
 };
 
 const Token = struct {
@@ -24,7 +25,7 @@ const Token = struct {
 };
 
 fn isspace(c: u8) bool {
-    return ((c == ' ') or (c == '\t'));
+    return ((c == ' ') or (c == '\t') or (c == '\n') or (c == '\r'));
 }
 
 fn isalpha(c: u8) bool {
@@ -43,80 +44,167 @@ fn isalphanum(c: u8) bool {
     return (isalpha(c) or isdigit(c));
 }
 
-pub fn gettok(buffer: []const u8, ptr0: u16) Token {
-    var ptr = ptr0;
-    const len: u16 = @intCast(buffer.len);
-    if (ptr >= len) return Token{ .s = ptr0, .e = ptr, .tag = .eof };
-    var lastchar = buffer[ptr];
+fn isnewline(c: u8) bool {
+    return ((c == '\n') or (c == '\r'));
+}
 
-    while (isspace(lastchar)) {
-        ptr += 1;
-        if (ptr < buffer.len) {
-            lastchar = buffer[ptr];
+const Lexer = struct {
+    buf: []const u8,
+    i: u16 = 0,
+    char: u8 = 0,
+
+    const Self = @This();
+    pub fn create(buf: []const u8) Self {
+        var lexer: Self = .{.buf = buf};
+        if (buf.len > 0) {
+            lexer.char = buf[0];
+        }
+
+        return lexer;
+    }
+
+    pub fn next_token(self: *Self) Token {
+        self.skip_whitechars();
+        
+        const token: Token = switch(self.char) {
+            0 => .{.s = self.i, .e = self.i, .tag = .eof},
+            '#' => cmt: {
+                self.skip_comment();
+                break :cmt self.next_token();
+            },
+            else => blk: {
+                if (isalpha(self.char)) {
+                    const ident = self.read_identifier();
+                    if (self.kw_equal(ident, "def")) break :blk .{.s = ident.s, .e = ident.e, .tag = .def};
+                    if (self.kw_equal(ident, "extern")) break :blk .{.s = ident.s, .e = ident.e, .tag = .tag_extern};
+                    break :blk ident;
+                }
+                if (isnum(self.char)) {
+                    break :blk self.read_number();
+                }
+
+                self.next_char();
+                break :blk .{.s = self.i - 1, .e = self.i, .tag = .unknown};
+            }
+        };
+
+        return token;
+    }
+
+    fn skip_whitechars(self: *Self) void {
+        while (isspace(self.char)) {
+            self.next_char();
+        }
+    }
+
+    fn next_char(self: *Self) void {
+        self.i += 1;
+        if (self.i >= self.buf.len) {
+            self.char = 0;
         } else {
-            return Token{ .s = ptr0, .e = ptr, .tag = .eof };
+            self.char = self.buf[self.i];
         }
     }
 
-    if (isalpha(lastchar)) {
-        while (isalphanum(lastchar)) {
-            ptr += 1;
-            if (ptr >= len) {
-                ptr = len;
-                break;
-            }
-            lastchar = buffer[ptr];
+    fn read_identifier(self: *Self) Token {
+        std.debug.assert(isalpha(self.char));
+        const pos = self.i;
+        while (isalphanum(self.char)) {
+            self.next_char();
         }
 
-        if (std.mem.eql(u8, buffer[ptr0..ptr], "def")) return Token{ .s = ptr0, .e = ptr, .tag = .def };
-        if (std.mem.eql(u8, buffer[ptr0..ptr], "extern")) return Token{ .s = ptr0, .e = ptr, .tag = .tag_extern };
-        return Token{ .s = ptr0, .e = ptr, .tag = .identifier };
+        return .{.s = pos, .e = self.i, .tag = .identifier};
     }
 
-    if (isnum(lastchar)) {
-        while (isnum(lastchar)) {
-            ptr += 1;
-            if (ptr >= len) {
-                ptr = len;
-                break;
-            }
-            lastchar = buffer[ptr];
+    fn skip_comment(self: *Self) void {
+        while (!(isnewline(self.char)) and self.char != 0) {
+            self.next_char();
+        }
+    }
+
+    fn read_number(self: *Self) Token {
+        std.debug.assert(isnum(self.char));
+        const pos = self.i;
+        while (isnum(self.char)) {
+            self.next_char();
         }
 
-        return Token{ .s = ptr0, .e = ptr, .tag = .number };
+        return .{.s = pos, .e = self.i, .tag = .number};
     }
 
-    if (lastchar == '#') {
-        while (true) {
-            ptr += 1;
-            if ((lastchar == '\n') or (lastchar == '\r') or (ptr >= len)) break;
-            lastchar = buffer[ptr];
-        }
-
-        if (ptr >= len) return Token{ .s = ptr0, .e = len - 1, .tag = .eof };
-        return gettok(buffer, ptr);
+    fn kw_equal(self: Self, token: Token, kw: []const u8) bool {
+        return std.mem.eql(u8, self.buf[token.s..token.e], kw);
     }
 
-    return Token{ .s = ptr0, .e = ptr, .tag = .regular };
+    fn inspect(self: Self, token: Token) []const u8 {
+        return self.buf[token.s..token.e];
+    }
+};
+
+
+test "identifier" {
+    const buffer = "fib";
+    var lexer = Lexer.create(buffer);
+
+    const token = lexer.next_token();
+    try testing.expect(token.tag == .identifier);
+    try testing.expectEqualStrings(lexer.inspect(token), "fib");
+
+    const token_eof = lexer.next_token();
+    try testing.expect(token_eof.tag == .eof);
 }
 
-test "Def token" {
+test "def" {
+    const expected: [5] struct {[]const u8, Tag} = .{
+        .{"def", .def},
+        .{"fib", .identifier},
+        .{"(", .unknown},
+        .{"x", .identifier},
+        .{")", .unknown},
+    };
     const buffer = "def fib(x)";
-    const token = gettok(buffer, 0);
+    var lexer = Lexer.create(buffer);
 
-    try testing.expect(token.tag == .def);
+    for (expected) |p| {
+        const token = lexer.next_token();
+        try testing.expect(token.tag == p.@"1");
+        try testing.expectEqualStrings(lexer.inspect(token), p.@"0");
+    }
+    const token_eof = lexer.next_token();
+    try testing.expect(token_eof.tag == .eof);
 }
 
-test "comment token" {
-    const buffer = "#foo\n#bar";
-    const token = gettok(buffer, 0);
+test "numbers" {
+    const expected: [3] struct {[]const u8, Tag} = .{
+        .{"35", .number},
+        .{"foo2", .identifier},
+        .{"29.48", .number},
+    };
+    const buffer = "35 foo2   29.48";
+    var lexer = Lexer.create(buffer);
 
-    try testing.expect(token.tag == .eof);
+    for (expected) |p| {
+        const token = lexer.next_token();
+        try testing.expect(token.tag == p.@"1");
+        try testing.expectEqualStrings(lexer.inspect(token), p.@"0");
+    }
+    const token_eof = lexer.next_token();
+    try testing.expect(token_eof.tag == .eof);
 }
 
-test "alphanum token" {
-    const buffer = "123.456 foo";
-    const token = gettok(buffer, 0);
+test "comments" {
+    const expected: [2] struct {[]const u8, Tag} = .{
+        .{"foo", .identifier},
+        .{"bar", .identifier},
+    };
+    const buffer = "foo # my comment\n# another comment\n\nbar";
+    var lexer = Lexer.create(buffer);
 
-    try testing.expect(token.tag == .number);
+    for (expected) |p| {
+        const token = lexer.next_token();
+        try testing.expect(token.tag == p.@"1");
+        try testing.expectEqualStrings(lexer.inspect(token), p.@"0");
+    }
+    const token_eof = lexer.next_token();
+    try testing.expect(token_eof.tag == .eof);
 }
